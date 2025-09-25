b7191c6ddb4f20f7c7eddffe3f5a71d39a48c6d8
TaewonYu17 <TaewonYu17@gmail.com>
2025-09-25 13:25:17 -0400

    Update scraping logic

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 forums = []
 pages = []
+posts = []
+#Get all subforum links
 links = soup.find_all('a', class_= 'forumtitle')
 for link in links:
     href = re.sub(r'&sid=[^&]+', '', link['href'])
     href = href.lstrip('.')
     full_url = f"https://www.norduserforum.com{href}"
     forums.append(full_url)
-    print(full_url)
+    # print(full_url)
 
-
+#get all pages links from each subforum
 for link in forums:
     response = requests.get(link)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -27,8 +29,24 @@ for link in forums:
     for topic_num in page:
         match = re.search(r"(\d+) topics", topic_num)
         page_num= math.ceil(int(match.group(1))/50)
-    print(page_num)
-    for i in range(1,page_num):
+    #print(page_num)
+    for i in range(0,page_num):
         pages.append(f"{link}&start={i*50}")
-print(pages)
 print(len(pages))
+
+#Get all post links from each page
+for page in pages:
+    response = requests.get(page)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    titles = soup.find_all('a', class_='topictitle')
+    for idx, title in enumerate(titles):
+        if idx >= 1:
+            break
+        href = title['href']
+        href = href.lstrip('.')
+        full_url = f"https://www.norduserforum.com{href}"
+        print(full_url)
+        posts.append(full_url)
+print(len(posts))
+
+
